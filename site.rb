2e9829d87f0ca00b794a1302bb1a3a878b953a11
Mason Gup <masongup@gmail.com>
2015-11-01 12:18:49 -0600

    Rework site for more production-like
    Validate the NODM, display an error if bad
    Set a timeout on the web request
    Set an exception handler on the whole request-parse part
    Return an error and log the exception on any type of request or parse failure
    Refactor to a single processing method

--- site.rb
+++ site.rb
@@ -3,29 +3,36 @@ require 'nokogiri'
 require 'rest-client'
 
 get('/') do
-	'Hello, World!'
+  'Hello, World!'
 end
 
 get('/search_nodm') do
-	erb(:search_nodm)
-end
+  nodm_input = params['nodm']
 
-get '/do_search_nodm' do
-	nodm_input = params['nodm']
-  
-	params = { 
-		method: 'displayByICAOs', 
-		reportType: 'RAW', 
-		formatType: 'DOMESTIC',
-		retrieveLocId: nodm_input, 
-		actionType: 'notamRetrievalByICAOs' 
-		}
+  if !nodm_input
+    erb(:search_nodm, { locals: { site_error: nil } } )
+  elsif /\A[A-Za-z]{3,4}\z/ === nodm_input
+    params = { 
+      method: 'displayByICAOs', 
+      reportType: 'RAW', 
+      formatType: 'DOMESTIC',
+      retrieveLocId: nodm_input, 
+      actionType: 'notamRetrievalByICAOs' 
+      }
 
-	url = 'https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do'
-	result = RestClient.get(url, { params: params })
-	parsed = Nokogiri::HTML(result)
+    begin
+      url = 'https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do'
+      result = RestClient::Request.execute(method: :get, url: url, timeout: 5, headers: { params: params })
+      parsed = Nokogiri::HTML(result)
 
-	norm_results = parsed.css('#resultsHomeLeft #notamRight span').map { |x| x.text.gsub('!', '') }
-  
-	erb(:do_search_nodm, { locals: { data: norm_results } } )
+      norm_results = parsed.css('#resultsHomeLeft #notamRight span').map { |x| x.text.gsub('!', '') }
+      
+      erb(:do_search_nodm, { locals: { data: norm_results } } )
+    rescue => e
+      logger.error e.to_s
+      erb(:search_nodm, { locals: { site_error: 'An error occured trying to retrieve the NODMs' } } )
+    end
+  else
+    erb(:search_nodm, { locals: { site_error: 'Invalid ICAO code' } } )
+  end
 end
