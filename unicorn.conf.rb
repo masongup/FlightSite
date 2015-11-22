794652816365f5eac2ca0cfc904141fdc994b8cd
Mason Gup <masongup@gmail.com>
2015-11-22 15:34:47 -0600

    Set up unicorn conf for simpler dev setup

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,9 +1,12 @@
 listen '127.0.0.1:4567'
 preload_app false
-worker_processes 2
 timeout 10
-pid 'tmp/unicorn.pid'
 
-log_path =  '/var/unicorn_logs/log.txt'
-stderr_path log_path 
-stdout_path log_path 
+if ENV['RAILS_ENV'] == 'production'
+  worker_processes 2
+  pid 'tmp/unicorn.pid'
+
+  log_path =  '/var/unicorn_logs/log.txt'
+  stderr_path log_path 
+  stdout_path log_path 
+end
