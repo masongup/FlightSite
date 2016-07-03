5282faf03caa18873d5482e13aa5773b0119f08f
Mason Gup <masongup@gmail.com>
2016-07-03 18:54:06 -0500

    Configure production unicorn
    Set the worker user to an unprivileged user

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -6,6 +6,8 @@ if ENV['RACK_ENV'] == 'production'
   worker_processes 2
   pid 'tmp/unicorn.pid'
 
+  user 'unicorn_worker', 'unicorn_worker'
+
   log_path =  '/var/unicorn_logs/log.txt'
   stderr_path log_path 
   stdout_path log_path 
