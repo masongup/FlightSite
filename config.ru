7f3c3ccca0cc2f65584fdfa6c65d3487ed921ef1
Mason Gup <masongup@gmail.com>
2016-08-28 18:30:37 -0400

    Set up app to run as subclass for joining with Rails

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
 require './site.rb'
-run Sinatra::Application
+run FlightSite
--- site.rb
+++ site.rb
@@ -1,46 +1,48 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'nokogiri'
 require 'rest-client'
 require_relative 'date_compute'
 
-get('/') do
-  erb(:site_functions, { locals: { errors: { } } } )
-end
-
-get('/SearchNodm') do
-  nodm_input = params['nodm']
-
-  if !nodm_input
+class FlightSite < Sinatra::Base
+  get('/') do
     erb(:site_functions, { locals: { errors: { } } } )
-  elsif /\A[A-Za-z]{3,4}\z/ === nodm_input
-    params = { 
-      method: 'displayByICAOs', 
-      reportType: 'RAW', 
-      formatType: 'DOMESTIC',
-      retrieveLocId: nodm_input, 
-      actionType: 'notamRetrievalByICAOs' 
-      }
-
-    begin
-      url = 'https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do'
-      result = RestClient::Request.execute(method: :get, url: url, timeout: 5, headers: { params: params })
-      parsed = Nokogiri::HTML(result)
-
-      norm_results = parsed.css('#resultsHomeLeft #notamRight span').map { |x| x.text.gsub('!', '') }
-      
-      erb(:do_search_nodm, { locals: { data: norm_results, nodm: nodm_input } } )
-    rescue => e
-      logger.error e.to_s
-      erb(:site_functions, { locals: { errors: { nodm_error: 'An error occured trying to retrieve the NODMs' } } } )
-    end
-  else
-    erb(:site_functions, { locals: { errors: { nodm_error: 'Invalid ICAO code' } } } )
   end
-end
 
-get('/ConvertTime') do
-  depTimeStr = params['DepTime']
-  eteStr = params['ETE']
-  etaStr = DateCompute.convert_time(depTimeStr, eteStr)
-  erb(:do_convert_time, { locals: { depTime: depTimeStr, ete: eteStr, eta: etaStr } } )
+  get('/SearchNodm') do
+    nodm_input = params['nodm']
+
+    if !nodm_input
+      erb(:site_functions, { locals: { errors: { } } } )
+    elsif /\A[A-Za-z]{3,4}\z/ === nodm_input
+      params = { 
+        method: 'displayByICAOs', 
+        reportType: 'RAW', 
+        formatType: 'DOMESTIC',
+        retrieveLocId: nodm_input, 
+        actionType: 'notamRetrievalByICAOs' 
+        }
+
+      begin
+        url = 'https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do'
+        result = RestClient::Request.execute(method: :get, url: url, timeout: 5, headers: { params: params })
+        parsed = Nokogiri::HTML(result)
+
+        norm_results = parsed.css('#resultsHomeLeft #notamRight span').map { |x| x.text.gsub('!', '') }
+        
+        erb(:do_search_nodm, { locals: { data: norm_results, nodm: nodm_input } } )
+      rescue => e
+        logger.error e.to_s
+        erb(:site_functions, { locals: { errors: { nodm_error: 'An error occured trying to retrieve the NODMs' } } } )
+      end
+    else
+      erb(:site_functions, { locals: { errors: { nodm_error: 'Invalid ICAO code' } } } )
+    end
+  end
+
+  get('/ConvertTime') do
+    depTimeStr = params['DepTime']
+    eteStr = params['ETE']
+    etaStr = DateCompute.convert_time(depTimeStr, eteStr)
+    erb(:do_convert_time, { locals: { depTime: depTimeStr, ete: eteStr, eta: etaStr } } )
+  end
 end
