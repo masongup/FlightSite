96c6a3f4a73b4c82738a9f558230f751ae8b9ef7
Mason Gup <masongup@gmail.com>
2023-07-06 00:02:44 -0400

    Fix NODM retrieval, other cleanups

--- Gemfile
+++ Gemfile
@@ -3,12 +3,11 @@ source 'http://rubygems.org'
 ruby '3.2.2'
 
 gem 'roda', '~> 3.69'
-gem 'nokogiri', '~> 1.15'
-gem 'rest-client', '~> 2.1'
 gem 'rake', '~> 13.0'
 gem 'puma', '~> 6.3'
 gem 'tilt', '~> 2.2.0'
 gem 'haml', '~> 6.1'
+gem 'faraday', '~> 2.7.9'
 
 group :development do
   gem 'rerun'
--- site.rb
+++ site.rb
@@ -1,14 +1,17 @@
 require 'roda'
-require 'nokogiri'
-require 'rest-client'
+require 'faraday'
+require 'json'
+require 'securerandom'
 require_relative 'date_compute'
 
 class FlightSite < Roda
   plugin :render, engine: 'haml'
+  plugin :sessions, secret: SecureRandom.alphanumeric(64)
+  plugin :flash
 
   route do |r|
     r.root do
-      view('site_functions', { locals: { errors: { } } } )
+      view('site_functions', { locals: { error: flash['error'] } } )
     end
 
     r.get 'SearchNodm' do
@@ -17,27 +20,25 @@ class FlightSite < Roda
       if !nodm_input
         view('site_functions', { locals: { errors: { } } } )
       elsif /\A[A-Za-z]{3,4}\z/ === nodm_input
-        params = { 
-          method: 'displayByICAOs', 
-          reportType: 'RAW', 
-          formatType: 'DOMESTIC',
-          retrieveLocId: nodm_input, 
-          actionType: 'notamRetrievalByICAOs' 
-          }
+        params = {
+          designatorsForLocation: nodm_input,
+          searchType: 0,
+          notamsOnly: true
+        }
 
         begin
-          url = 'https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do'
-          result = RestClient::Request.execute(method: :get, url: url, timeout: 5, headers: { params: params })
-          parsed = Nokogiri::HTML(result)
-
-          norm_results = parsed.css('#resultsHomeLeft #notamRight span').map { |x| x.text.gsub('!', '') }
-          
-          view('do_search_nodm', { locals: { data: norm_results, nodm: nodm_input } } )
+          url = 'https://notams.aim.faa.gov/notamSearch/search'
+          result = Faraday.post(url, params)
+          parsed = JSON.parse(result.body)
+          norm_results = parsed.map { |nr| nr['traditionalMessageFrom4thWord'] }
+          view('do_search_nodm', { locals: { data: norm_results, nodm: nodm_input } })
         rescue => e
-          view('site_functions', { locals: { errors: { nodm_error: 'An error occured trying to retrieve the NODMs' } } } )
+          flash['error'] = 'An error occured trying to retrieve the NODMs'
+          r.redirect '/'
         end
       else
-        view('site_functions', { locals: { errors: { nodm_error: 'Invalid ICAO code' } } } )
+        flash['error'] = 'Invalid ICAO code'
+        r.redirect '/'
       end
     end
 
